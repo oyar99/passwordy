dbf90d236dd18fc9dcf5655505d769bdf425f862
Jhon Rayo <rayojhon99@gmail.com>
2021-05-16 10:57:45 -0500

    update karma conf js

--- projects/ngx-passwordy-test/karma.conf.js
+++ projects/ngx-passwordy-test/karma.conf.js
@@ -25,8 +25,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browsers: ['Chrome', 'ChromeHeadlessCI'],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    }
   });
 };
